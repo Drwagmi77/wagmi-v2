f5adedd0a87bcb620043482e750029a6f4d9a0f7
Drwagmi77 <smhp77emn@gmail.com>
2025-07-17 06:55:06 +0300

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -1,3 +1,3 @@
-ALTER TABLE wagmi_temp_verifications ADD COLUMN wallet_address VARCHAR;
+ALTER TABLE wagmi_temp_verifications ADD COLUMN IF NOT EXISTS wallet_address VARCHAR;
 ALTER TABLE wagmi_memberships ALTER COLUMN user_id TYPE BIGINT;
 ALTER TABLE wagmi_temp_verifications ALTER COLUMN user_id TYPE BIGINT;
